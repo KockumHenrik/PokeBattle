e6b56ead344f4de6d0214a015969d63bab7c0e21
KockumHenrik <henkekockum@gmail.com>
2020-10-27 01:20:56 +0100

    Fixed faulty remove method

--- src/Objects/MoveSet.java
+++ src/Objects/MoveSet.java
@@ -1,27 +1,48 @@
 package Objects;
 
+import java.util.ArrayList;
+
 public class MoveSet {
 	public Move[] set = new Move[4];
 	public int size = 0;
 	
-	public void add(Move move) {
+	public ArrayList<Move> toMoves(){
+		ArrayList<Move> temp = new ArrayList<>();
+		for(int i = 0; i < size; i++) {
+			temp.add(set[i]);
+		}
+		return temp;
+	}
+	
+	public ArrayList<String> toNameOfMoves(){
+		ArrayList<String> temp = new ArrayList<>();
+		for(int i = 0; i < size; i++) {
+			temp.add(set[i].getName());
+		}
+		return temp;
+	}
+	
+	public void add(Move move) {		
 		set[size] = move;
 		size++;
 	}
 	
 	public void remove(int index) {
-		if(size == 0) {
-			
+		if(size == 0) {			
 		}
 		else if(index > size) {
-			
 		}
 		else {
-			for(int i = index; i < size-1; i++) {
-				set[i] = set[i+1];
-				set[i+1] = null;
+			int i;
+			for(i = index; i < size-1; i++) {
+				set[i] = set[i+1];				
 			}
-		}
-		
+			set[i] = null;
+			size--;			
+		}		
+	}
+	
+	public String toString() {
+		return String.format("[%s , %s , %s , %s]", set[0].getName(), set[1].getName(), set[2].getName(), set[3].getName());
 	}
 }
